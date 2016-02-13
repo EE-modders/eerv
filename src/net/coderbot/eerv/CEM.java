e440d07bb66ac72c7eff68ffd6065f5ef4e7ef57
coderbot <22420959+coderbot16@users.noreply.github.com>
2016-02-12 22:22:54 -0800

    Full loading of CEM 2.0 CEM 1.0 (legacy) is not supported yet.

---
+++ src/net/coderbot/eerv/CEM.java
@@ -0,0 +1,17 @@
+package net.coderbot.eerv;
+
+import net.coderbot.math.Vector3;
+
+public class CEM 
+{
+	String misc;
+	Vector3 center;
+	int totalPolygons;
+	int[][] indices0;
+	int[][] indices1;
+	int[][] indices2;
+	CEMMaterial[] materials;
+	String[] tagPointNames;
+	CEMFrame[] frames;
+	int subModels;
+}
---
+++ src/net/coderbot/eerv/CEMMaterial.java
@@ -0,0 +1,13 @@
+package net.coderbot.eerv;
+
+public class CEMMaterial 
+{
+	String name;
+	String name2;
+	
+	int textureIndex;
+	int[] polygonStart;
+	int[] polygonLength;
+	int vertexStart;
+	int vertexLength;
+}
